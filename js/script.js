8611b6ace0c6dd13919d2f36e883c96b2b8a85cc
SofiaMoshe <moshe.sofia@gmail.com>
2022-10-06 22:12:40 +0300

    Adding showPage and addPagination functions

--- js/script.js
+++ js/script.js
@@ -15,9 +15,43 @@ For assistance:
 
 /*
 Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+Function 'showPage will create the html elements needed to display the page with nine students
 */
 
+//showPage function -  will have two parameters-
+//  'list' for student date - 
+//  'page' for page number 
+//  - both passed as an arugument when the function is called
+function showPage(list, page) {
+
+
+	//Two variables which will display start and end index of student data 
+	const startIndex = (page * 9) - 9;
+	const endIndex = page * 9;
+
+	const studentList = document.querySelector(".student-list");
+
+	studentList.innerHTML = "";
+	let html = "";
+
+	for (let i = 0; i < list.length; i++) {
+		if (i >= startIndex && i < endIndex) {
+			html += `
+        <li class="student-item cf">
+          <div class="student-details">
+            <img class="avatar" src= ${list[i].picture.large} alt="Profile Picture">
+            <h3>${list[i].name.first} ${list[i].name.last}</h3>
+            <span class="email">${list[i].email}</span>
+          </div>
+          <div class="joined-details">
+            <span class="date">Joined ${list[i].registered.date}</span>
+          </div>
+        </li>
+          `;
+		}
+	}
+	studentList.insertAdjacentHTML("beforeend", html);
+}
 
 
 /*
@@ -25,6 +59,39 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list) {
+	// variable to store the value of the number of pagination buttons needed
+	const numOfPages = Math.ceil(list.length / 9);
 
+	const linkList = document.querySelector(".link-list");
+	//the list is set to am empty string to remove data if present
+	linkList.innerHTML = "";
+	let buttonHTML = "";
+
+	for (let i = 1; i <= numOfPages; i++) {
+		buttonHTML += `
+     <li>
+       <button type="button">${i}</button>
+     </li>    
+     `;
+	}
+
+	linkList.insertAdjacentHTML("beforeend", buttonHTML);
+
+	const button = document.querySelectorAll("button[type='button']");
+	button[0].className = "active";
+
+	linkList.addEventListener("click", (e) => {
+		const buttonClicked = e.target;
+		if (buttonClicked.tagName === "BUTTON") {
+			const activeClassButton = document.getElementsByClassName("active");
+			activeClassButton[0].className = "";
+			buttonClicked.className = "active";
+			showPage(list, buttonClicked.textContent);
+		}
+	});
+}
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
